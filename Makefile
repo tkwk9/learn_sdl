60c8e6a3a09ff30ede39eb5d2d8ee954d8660266
tkwk9 <tim.k.kwak@gmail.com>
2024-05-21 18:24:35 -0700

    add circle around mouse

--- Makefile
+++ Makefile
@@ -6,19 +6,19 @@ GLAD_INCLUDE_DIR = ./glad/include
 all: $(APP_NAME)
 
 $(APP_NAME): main.o glad.o
-	clang++ main.o glad.o -o $(APP_NAME) -F$(FRAMEWORKS_DIR) -framework SDL2 -Wl,-rpath,$(FRAMEWORKS_DIR)
+	clang++ -std=c++11 main.o glad.o -o $(APP_NAME) -F$(FRAMEWORKS_DIR) -framework SDL2 -Wl,-rpath,$(FRAMEWORKS_DIR) -framework OpenGL
 
 main.o: main.cpp
-	clang++ -c main.cpp -o main.o -I$(FRAMEWORKS_DIR)/SDL2.framework/Headers -I$(GLAD_INCLUDE_DIR) -F$(FRAMEWORKS_DIR) -framework SDL2
+	clang++ -std=c++11 -c main.cpp -o main.o -I$(FRAMEWORKS_DIR)/SDL2.framework/Headers -I$(GLAD_INCLUDE_DIR) -F$(FRAMEWORKS_DIR) -framework SDL2
 
 glad.o: $(GLAD_SRC_DIR)/glad.c
 	clang++ -c $(GLAD_SRC_DIR)/glad.c -o glad.o -I$(GLAD_INCLUDE_DIR)
 
 debug: main_debug.o glad_debug.o
-	g++ -g main_debug.o glad_debug.o -o $(APP_NAME) -F$(FRAMEWORKS_DIR) -framework SDL2 -Wl,-rpath,$(FRAMEWORKS_DIR)
+	g++ -std=c++11 -g main_debug.o glad_debug.o -o $(APP_NAME) -F$(FRAMEWORKS_DIR) -framework SDL2 -Wl,-rpath,$(FRAMEWORKS_DIR) -framework OpenGL
 
 main_debug.o: main.cpp
-	g++ -g -c main.cpp -o main_debug.o -I$(FRAMEWORKS_DIR)/SDL2.framework/Headers -I$(GLAD_INCLUDE_DIR) -F$(FRAMEWORKS_DIR) -framework SDL2
+	g++ -std=c++11 -g -c main.cpp -o main_debug.o -I$(FRAMEWORKS_DIR)/SDL2.framework/Headers -I$(GLAD_INCLUDE_DIR) -F$(FRAMEWORKS_DIR) -framework SDL2
 
 glad_debug.o: $(GLAD_SRC_DIR)/glad.c
 	g++ -g -c $(GLAD_SRC_DIR)/glad.c -o glad_debug.o -I$(GLAD_INCLUDE_DIR)
--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <cstdlib>
+#include <cmath>
+#include <vector>
 
 #include <SDL2/SDL.h>
 #include <glad/glad.h>
@@ -7,56 +9,200 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
+const char* vertexShaderSource = R"(
+#version 330 core
+layout(location = 0) in vec2 position;
+void main()
+{
+    gl_Position = vec4(position, 0.0, 1.0);
+}
+)";
+
+const char* fragmentShaderSource = R"(
+#version 330 core
+out vec4 FragColor;
+void main()
+{
+    FragColor = vec4(1.0, 0.0, 0.0, 1.0); // Red color
+}
+)";
+
+void drawCircle(float cx, float cy, float r, int num_segments, GLuint shaderProgram)
+{
+    std::vector<float> vertices;
+    for (int i = 0; i < num_segments; ++i)
+    {
+        float theta = 2.0f * 3.1415926f * float(i) / float(num_segments); // get the current angle
+        float x = r * cosf(theta); // calculate the x component
+        float y = r * sinf(theta); // calculate the y component
+        vertices.push_back(x + cx);
+        vertices.push_back(y + cy);
+    }
+
+    GLuint VAO, VBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glUseProgram(shaderProgram);
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_LINE_LOOP, 0, num_segments);
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+}
+
+GLuint compileShader(GLenum type, const char* source)
+{
+    GLuint shader = glCreateShader(type);
+    glShaderSource(shader, 1, &source, nullptr);
+    glCompileShader(shader);
+
+    int success;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        char infoLog[512];
+        glGetShaderInfoLog(shader, 512, nullptr, infoLog);
+        std::cerr << "ERROR::SHADER::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    return shader;
+}
+
+GLuint createShaderProgram(const char* vertexSource, const char* fragmentSource)
+{
+    GLuint vertexShader = compileShader(GL_VERTEX_SHADER, vertexSource);
+    GLuint fragmentShader = compileShader(GL_FRAGMENT_SHADER, fragmentSource);
+
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    int success;
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success)
+    {
+        char infoLog[512];
+        glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
+        std::cerr << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    return shaderProgram;
+}
+
 int main(int argc, char *argv[])
 {
     // The window we'll be rendering to
     SDL_Window *window = NULL;
+    SDL_GLContext context;
 
     // Initialize SDL
     if (SDL_Init(SDL_INIT_VIDEO) < 0)
     {
         std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
+        return 1;
     }
-    else
+
+    // Set OpenGL attributes
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+    // Create window
+    window = SDL_CreateWindow("SDL OpenGL",
+                              SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                              SCREEN_WIDTH, SCREEN_HEIGHT,
+                              SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
+    if (window == NULL)
     {
-        // Create window
-        window = SDL_CreateWindow("SDL Tutorial",
-                                  SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-                                  SCREEN_WIDTH, SCREEN_HEIGHT,
-                                  SDL_WINDOW_SHOWN);
-        if (window == NULL)
-        {
-            std::cerr << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
-        }
-        else
-        {
-            // Get window surface
-            SDL_Surface *screenSurface = SDL_GetWindowSurface(window);
-
-            // Fill the surface white
-            SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
-
-            // Update the surface
-            SDL_UpdateWindowSurface(window);
-
-            // Hack to get window to stay up
-            SDL_Event e;
-            bool quit = false;
-            while (!quit)
-            {
-                while (SDL_PollEvent(&e))
-                {
-                    if (e.type == SDL_QUIT)
-                        quit = true;
-                }
-            }
-
-            // Destroy window
-            SDL_DestroyWindow(window);
-        }
+        std::cerr << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
     }
 
-    // Quit SDL subsystems
+    // Create context
+    context = SDL_GL_CreateContext(window);
+    if (context == NULL)
+    {
+        std::cerr << "OpenGL context could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    // Initialize GLAD
+    if (!gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress))
+    {
+        std::cerr << "Failed to initialize GLAD" << std::endl;
+        SDL_GL_DeleteContext(context);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    // Set the viewport
+    glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+    // Create shader program
+    GLuint shaderProgram = createShaderProgram(vertexShaderSource, fragmentShaderSource);
+
+    // Main loop flag
+    bool quit = false;
+
+    // Event handler
+    SDL_Event e;
+
+    // Enable blending
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    // While application is running
+    while (!quit)
+    {
+        // Handle events on queue
+        while (SDL_PollEvent(&e) != 0)
+        {
+            // User requests quit
+            if (e.type == SDL_QUIT)
+            {
+                quit = true;
+            }
+        }
+
+        // Get mouse position
+        int mouseX, mouseY;
+        SDL_GetMouseState(&mouseX, &mouseY);
+
+        // Clear color buffer
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Set the clear color to white
+        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+
+        // Draw a circle around the mouse cursor
+        float normalizedX = (2.0f * mouseX) / SCREEN_WIDTH - 1.0f;
+        float normalizedY = 1.0f - (2.0f * mouseY) / SCREEN_HEIGHT;
+        drawCircle(normalizedX, normalizedY, 0.1f, 100, shaderProgram);
+
+        // Update window with OpenGL rendering
+        SDL_GL_SwapWindow(window);
+    }
+
+    // Cleanup
+    glDeleteProgram(shaderProgram);
+    SDL_GL_DeleteContext(context);
+    SDL_DestroyWindow(window);
     SDL_Quit();
 
     return 0;
